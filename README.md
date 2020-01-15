dda79f11217c65931b53e0e87a3e445a26a0e604
zareen27 <zareenfariha027@gmail.com>
2020-01-15 11:09:53 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# GolpoKotha
+ An app which stores family memories in the form of photo album and stories from older ones of the family.
---
+++ app/src/main/java/com/example/zareen_tasnim/golpokotha/Home.java
@@ -0,0 +1,13 @@
+package com.example.zareen_tasnim.golpokotha;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
